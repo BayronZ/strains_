d7b495dafaad460fc2f57a334ad55c26ba56b643
BayronZ <bayron.zavalag@gmail.com>
2022-06-21 00:30:57 -0400

    added filter and order by letter

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -2,6 +2,15 @@ class Strain < ApplicationRecord
   #Validations
   validates :name, presence: true, uniqueness: true
 
+  before_validation :sanitize
+
+  private
+  def sanitize
+    self.name = self.name.strip.camelize unless self.name.nil?
+  end
+
+
+
   #Associations
   has_many :wine_strains
   has_many :wines, through: :wine_strains
